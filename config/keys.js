917628dd532cbf1ee4933640d38aae23fe6316f3
SwathiRavi28 <68095540+SwathiRavi28@users.noreply.github.com>
2021-01-31 23:57:46 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Swathi:' + encodeURIComponent('1u0MiNtS5NWXrUa4') + '@cluster0.ann9b.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  secretOrKey: "secret"
+};
